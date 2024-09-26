ae9d29e6588e83b0e4966463048d5871f7d2e335
Vasyl Shaposhnyk <aries240302@gmail.com>
2024-09-26 13:53:56 +0300

    Add markup that saves in local storage and can add new notes

---
+++ index.js
@@ -0,0 +1,59 @@
+const input = document.querySelector('#input');
+const addBtn = document.querySelector('#plus-btn');
+const taskList = document.querySelector('#task-list');
+const tasksAmount = document.querySelector('#tasks-amount');
+const doneTasksAmount = document.querySelector('#done-tasks-amount');
+const check = document.querySelector('#check');
+const deleteBtn = document.querySelector('#delete');
+
+const taskListArr = [
+  { id: 1, task: 'To study React fundamentals' },
+  { id: 2, task: 'To study React fundamentals' },
+  { id: 3, task: 'To study React fundamentals' },
+  { id: 4, task: 'To study React fundamentals' },
+];
+
+const doneTask = [{ id: 4, task: 'To study React fundamentals' }];
+
+const existTasks = localStorage.getItem('tasks');
+
+if (!existTasks) {
+  localStorage.setItem('tasks', JSON.stringify(taskListArr));
+}
+const parsedTasks = JSON.parse(existTasks);
+
+const updateMarkup = (tasks) => {
+  const markup = tasks
+    .map(
+      (taskItem) => `<li class="task-item" key='${taskItem.id}'>
+          <p>${taskItem.task}</p>
+          <div class="icon-container">
+            <button class="task-btn" id="check">
+              <svg class="icon-check"><use href="./sprite.svg#icon-check"></use></svg>
+            </button>
+
+            <button class="task-btn" id="deleteBtn">
+              <svg class="icon-trash"><use href="./sprite.svg#icon-trash"></use></svg>
+            </button>
+          </div>
+        </li>`
+    )
+    .join('');
+
+  taskList.innerHTML = markup;
+  tasksAmount.textContent = tasks.length;
+};
+
+updateMarkup(parsedTasks);
+
+doneTasksAmount.textContent = doneTask.length;
+
+addBtn.addEventListener('click', () => {
+  const newTask = { id: Date.now(), task: input.value };
+
+  parsedTasks.push(newTask);
+  localStorage.setItem('tasks', JSON.stringify(parsedTasks));
+  updateMarkup(parsedTasks);
+
+  input.value = '';
+});
