00c67d52eedcb02592045374090d752fe752a0b8
Vasyl Shaposhnyk <aries240302@gmail.com>
2024-09-30 21:12:45 +0300

    Add delete and done function

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const input = document.querySelector('#input');
 const addBtn = document.querySelector('#plus-btn');
 const taskList = document.querySelector('#task-list');
 const tasksAmount = document.querySelector('#tasks-amount');
+const taskListDone = document.querySelector('#task-list-done');
 const doneTasksAmount = document.querySelector('#done-tasks-amount');
-const check = document.querySelector('#check');
-// const deleteBtn = document.querySelector('#deleteBtn');
 
 const taskListArr = [
   { id: 1, task: 'To study React fundamentals' },
@@ -12,10 +11,9 @@ const taskListArr = [
   { id: 3, task: 'To study React fundamentals' },
   { id: 4, task: 'To study React fundamentals' },
 ];
-const doneTask = [{ id: 4, task: 'To study React fundamentals' }];
+const doneTaskArr = [{ id: 5, task: 'To study React fundamentals' }];
 
 const existTasks = localStorage.getItem('tasks');
-
 if (!existTasks) {
   localStorage.setItem('tasks', JSON.stringify(taskListArr));
 }
@@ -27,7 +25,7 @@ const updateMarkup = (tasks) => {
       (taskItem) => `<li class="task-item" key='${taskItem.id}'>
           <p>${taskItem.task}</p>
           <div class="icon-container">
-            <button class="task-btn" id="check">
+            <button class="task-btn" id="check" key='${taskItem.id}'>
               <svg class="icon-check"><use href="./sprite.svg#icon-check"></use></svg>
             </button>
 
@@ -45,10 +43,10 @@ const updateMarkup = (tasks) => {
 
 updateMarkup(parsedTasks);
 
-doneTasksAmount.textContent = doneTask.length;
-
-addBtn.addEventListener('click', () => {
-  const newTask = { id: Date.now(), task: input.value };
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const inputText = input.value.length > 33 ? input.value.slice(0, 33) + '...' : input.value;
+  const newTask = { id: Date.now(), task: inputText };
 
   parsedTasks.push(newTask);
   localStorage.setItem('tasks', JSON.stringify(parsedTasks));
@@ -61,9 +59,94 @@ const onDelete = (e) => {
   const deleteBtn = e.target.closest('#deleteBtn');
 
   if (deleteBtn) {
-    const task = e.target.closest('.task-item').key;
-    console.log(task);
+    const taskId = Number(deleteBtn.getAttribute('key'));
+
+    parsedTasks.map((task) => {
+      if (taskId === task.id) {
+        const idxToDelete = parsedTasks.indexOf(task);
+
+        parsedTasks.splice(idxToDelete, 1);
+        localStorage.setItem('tasks', JSON.stringify(parsedTasks));
+        updateMarkup(parsedTasks);
+      }
+    });
+  }
+};
+taskList.addEventListener('click', onDelete);
+
+//
+//
+
+//
+
+//
+
+const doneTasks = localStorage.getItem('doneTasks');
+
+if (!doneTasks) {
+  localStorage.setItem('doneTasks', JSON.stringify(doneTaskArr));
+}
+const parsedDoneTasks = JSON.parse(doneTasks);
+
+const updateMarkupDoneTask = (doneTasks) => {
+  const markup = doneTasks
+    .map(
+      (taskItem) => `<li class="task-item-done" id="task-item-done" key='${taskItem.id}'>
+          ${taskItem.task}
+        </li>`
+    )
+    .join('');
+
+  taskListDone.innerHTML = markup;
+  doneTasksAmount.textContent = doneTasks.length;
+};
+
+updateMarkupDoneTask(parsedDoneTasks);
+
+const doneTask = (e) => {
+  const checkBtn = e.target.closest('#check');
+
+  if (checkBtn) {
+    const taskId = Number(checkBtn.getAttribute('key'));
+
+    parsedTasks.map((task) => {
+      if (taskId === task.id) {
+        parsedDoneTasks.push(task);
+        localStorage.setItem('doneTasks', JSON.stringify(parsedDoneTasks));
+        updateMarkupDoneTask(parsedDoneTasks);
+
+        const idxToDelete = parsedTasks.indexOf(task);
+
+        parsedTasks.splice(idxToDelete, 1);
+        localStorage.setItem('tasks', JSON.stringify(parsedTasks));
+        updateMarkup(parsedTasks);
+      }
+    });
   }
 };
 
-taskList.addEventListener('click', onDelete);
+taskList.addEventListener('click', doneTask);
+
+const reversal = (e) => {
+  const doneTask = e.target.closest('#task-item-done');
+
+  if (doneTask) {
+    const taskId = Number(doneTask.getAttribute('key'));
+
+    parsedDoneTasks.map((task) => {
+      if (taskId === task.id) {
+        parsedTasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(parsedTasks));
+        updateMarkup(parsedTasks);
+
+        const idxToDelete = parsedDoneTasks.indexOf(task);
+
+        parsedDoneTasks.splice(idxToDelete, 1);
+        localStorage.setItem('doneTasks', JSON.stringify(parsedDoneTasks));
+        updateMarkupDoneTask(parsedDoneTasks);
+      }
+    });
+  }
+};
+
+taskListDone.addEventListener('click', reversal);
